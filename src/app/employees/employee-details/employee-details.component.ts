968b05d38a5e10b3355b501256e9421bb279ffea
Hassan Ramadan <RmdanJrx@gmail.com>
2023-01-30 18:49:31 +0200

    Add employee details component

---
+++ src/app/employees/employee-details/employee-details.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Employee } from '../employee.model';
+
+@Component({
+  selector: 'app-employee-details',
+  templateUrl: './employee-details.component.html',
+  styleUrls: ['./employee-details.component.css'],
+})
+export class EmployeeDetailsComponent {
+  employee: Employee = {
+    id: 'df34-l34j-23j4-dsf8',
+    username: 'RmdanJr',
+    name: 'Hassan Ramadan',
+    birthDate: new Date(2000, 9, 1),
+    gender: 'MALE',
+    graduationDate: new Date(2022, 7, 1),
+    salary: { gross: 30000, net: 24500, insuranceAmount: 500 },
+    department: { id: 'sw-dev-department-id', name: 'Software Development' },
+    team: { id: 'mrc-team-id', name: 'MRC' },
+    expertises: [
+      { name: 'C++', level: 'EXPERT' },
+      { name: 'Java', level: 'NEWBIE' },
+    ],
+    manager: { id: 'ayman-shebl-employee-id', name: 'Ayman Shebl' },
+    managedDepartment: null,
+    managedTeam: null,
+    managedEmployees: [],
+  };
+}
