efd3c7ab0e645c9a7371d4ebbda3d88abefe2936
Hassan Ramadan <RmdanJrx@gmail.com>
2023-02-06 02:27:32 +0200

    Add spinner component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { ModalComponent } from './modal/modal.component';
 import { AlertComponent } from './alert/alert.component';
 import { CommonModule } from '@angular/common';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
-  declarations: [ModalComponent, AlertComponent],
-  exports: [ModalComponent, AlertComponent],
+  declarations: [ModalComponent, AlertComponent, SpinnerComponent],
+  exports: [ModalComponent, AlertComponent, SpinnerComponent],
   imports: [CommonModule],
   providers: [],
 })
